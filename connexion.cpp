41a257e56243d910d6f915ee6abe2989011d0fe8
ANJY1 <58262370+ANJY1@users.noreply.github.com>
2019-11-28 01:14:21 +0100

    Create connexion.cpp

---
+++ connexion.cpp
@@ -0,0 +1,19 @@
+#include "connexion.h"
+#include <QSqlError>
+Connexion::Connexion(){
+                      }
+bool Connexion::ouvrirConnexion()
+{bool test=false;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+                           db.setDatabaseName("source_de_donnees");
+                           db.setUserName("Yvanna");//inserer nom de l'utilisateur
+                           db.setPassword("esprit2");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+    test=true;
+
+else throw QString ("Erreur Paramétres"+db.lastError().text());
+return  test;
+}
+void Connexion::fermerConnexion()
+{db.close();}
